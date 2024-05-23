02abc2d1c33e2b37c5e7c0ec77969f40bec9dc12
Daniel Lubarov <daniel@lubarov.com>
2024-05-23 13:27:44 -0700

    Add factor bin

---
+++ src/bin/factor.rs
@@ -0,0 +1,32 @@
+use std::env;
+use std::str::FromStr;
+
+use num_bigint::BigUint;
+
+use nums::{Factorizer, FactorizerFromSplitter, MillerRabin, PollardRho, QuadraticSieve};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        println!("Usage: {} <algorithm> <number>", args[0]);
+        return;
+    }
+    let algorithm = args[1].as_str();
+    let n = BigUint::from_str(&args[2]).expect("Failed to parse number");
+
+    let primality_test = MillerRabin { error_bits: 128 };
+    let prime_factors = match algorithm {
+        "qsieve" => FactorizerFromSplitter {
+            primality_test,
+            composite_splitter: QuadraticSieve,
+        }
+        .prime_factors(&n),
+        "rho" => FactorizerFromSplitter {
+            primality_test,
+            composite_splitter: PollardRho,
+        }
+        .prime_factors(&n),
+        _ => panic!("Unrecognized algorithm: {}", algorithm),
+    };
+    println!("Factors: {:?}", prime_factors);
+}
